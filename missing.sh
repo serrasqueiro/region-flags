4ebf23cf2e0b5a6f11b3ba2b9b9057f97cfdfb34
Henrique Moreira <h@serrasqueiro.com>
2021-04-03 20:34:10 +0100

    Eurozone (EZ) -- flag of europe

--- missing.sh
+++ missing.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
 ./regions.py | while read x y; do test -f "png/$x.png" || echo $x $y; done
+
+# Now let's re-generate 'reference.md'
+export PYTHONPATH=$PYTHONPATH:src
+
+python3 src/packreg/tomarkdown.py .
+
